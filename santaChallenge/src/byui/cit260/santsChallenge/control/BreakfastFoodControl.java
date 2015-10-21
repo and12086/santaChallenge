7eabfc1a7e9193820412f00162b2a50936134620
and12_000 <and12_000@BYUI-Lap.attlocal.net>
2015-10-21 13:14:14 -0500

    Lisa and Wendy implemented the BreakfastFood control class, and test package.  Still need to fix error on line 37 with the function.

--- santaChallenge/src/byui/cit260/santsChallenge/control/BreakfastFoodControl.java
+++ santaChallenge/src/byui/cit260/santsChallenge/control/BreakfastFoodControl.java
@@ -11,4 +11,33 @@ package byui.cit260.santsChallenge.control;
  */
 public class BreakfastFoodControl {
     
+    public double calcCaloriesConsumed(double calorieCount1, double calorieCount2, double calorieCount3, double calorieCount4, double healthBenefit) {
+        
+ 
+        //consequence for poor food choices (pop-tart, Lucky Charms, and nothing)   
+        if (calorieCount1 == 1 || calorieCount1 == 5 ||calorieCount1 == 8) {
+            return healthBenefit = 50;
+        }
+        if (calorieCount2 == 1 || calorieCount2== 5 ||calorieCount2 == 8) {
+            return healthBenefit = 50;
+        }
+        if (calorieCount3 == 1 || calorieCount3 == 5 ||calorieCount3 == 8) {
+            return healthBenefit = 50;
+        }
+        if (calorieCount4 == 1 || calorieCount4 == 5 ||calorieCount4 == 8) {
+            return healthBenefit = 50;
+        }
+        
+        //no consequence because they made good choices
+        else {
+           return healthBenefit = 0;
+        }
+        
+        
+    double caloriesConsumed = (calorieCount1 + calorieCount2 + calorieCount3 + calorieCount4)- healthBenefit;
+    
+    
+    return caloriesConsumed;
+    }
+    
 }
---
+++ santaChallenge/test/byui/cit260/santsChallenge/control/BreakfastFoodControlTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.santsChallenge.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author and12_000
+ */
+public class BreakfastFoodControlTest {
+    
+    public BreakfastFoodControlTest() {
+    }
+
+    /**
+     * Test of calcCalorieCount method, of class BreakfastFoodControl.
+     */
+    @Test
+    public void testCalcCalorieCount() {
+        System.out.println("calcCalorieCount");
+        /******************************
+         * Test Case #1
+         ******************************/
+        System.out.println("\tTest case #1");
+        
+        //input values for test case 1
+        double calorieCount1 = 200.0;
+        double calorieCount2 = 80.0;
+        double calorieCount3 = 160.0;
+        double calorieCount4 = 250.0;
+        double healthBenefit = 50.0;
+        
+        //create instance o f BreakfastFoodControl class
+        BreakfastFoodControl instance = new BreakfastFoodControl();
+        double expResult = 640.0; //expected output for returned value
+        
+        //call function to run test
+        double result = instance.calcCaloriesConsumed(calorieCount1, calorieCount2, calorieCount3, calorieCount4, healthBenefit);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0.0);
+        
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
