80b4be617170d72d5fd63a6af72a97264a434695
and12_000 <and12_000@BYUI-Lap>
2015-11-28 23:45:17 -0600

    Merge origin/master
    Conflicts:
    	santaChallenge/src/byui/cit260/santaChallenge/control/GameControl.java
    	santaChallenge/src/byui/cit260/santaChallenge/control/MapControl.java
    	santaChallenge/src/byui/cit260/santaChallenge/view/FeedSantaView.java
    	santaChallenge/src/byui/cit260/santaChallenge/view/GameMenuView.java

--- santaChallenge/src/byui/cit260/santaChallenge/view/LocationsView.java
+++ santaChallenge/src/byui/cit260/santaChallenge/view/LocationsView.java
@@ -11,6 +11,41 @@ import java.io.Serializable;
  *
  * @author Wendy
  */
-public class LocationsView implements Serializable {
+public class LocationsView extends View {
+
+    public LocationsView() {
+        super("\n"
+                + "\n************************************"
+                + "\nX - pick an x coordinate"
+                + "\nY - pick a y coordinate"
+                + "\nE - Exit the game");
+    }
     
-}
+    @Override
+    public boolean doAction(Object obj) {
+         String value = (String) obj;
+       // value = value.toUpperCase();
+        
+        switch (value){
+            case "X"://start a new game
+                this.move();
+                break;
+            case "Y"://restart and existing game
+                this.move();
+                break;
+            case "E"://exit the program
+                System.out.println("\n*** Thank you for playing the Santa Challenge!");
+            default:
+                System.out.println("\n*** Invalid selection *** Try again!");
+                break;        
+        } 
+        return false;
+    }
+
+    private void move() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+ }
+    
